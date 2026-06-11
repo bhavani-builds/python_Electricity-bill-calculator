2055ad178951c89fde8ba301d218d587dc08bbec
NEELAPALA NAGA DURGA BHAVANI <bhavanineelapaladev01@gmail.com>
2026-06-11 21:41:42 +0530

    Add files via upload

---
+++ electricty bill.py
@@ -0,0 +1,16 @@
+# Electricity Bill Calculation
+
+units = float(input("Enter electricity units consumed: "))
+
+bill = 0
+
+if units <= 100:
+    bill = units * 1.5
+elif units <= 200:
+    bill = (100 * 1.5) + ((units - 100) * 2.5)
+elif units <= 300:
+    bill = (100 * 1.5) + (100 * 2.5) + ((units - 200) * 4)
+else:
+    bill = (100 * 1.5) + (100 * 2.5) + (100 * 4) + ((units - 300) * 6)
+
+print(f"Electricity Bill = ₹{bill:.2f}")
